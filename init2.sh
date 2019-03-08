3b3867242b48dc52d8561383cc34159681cfa9d5
A.I <ailis@paw.zone>
2019-03-07 16:10:59 -0800

    Mount the local folder at /vagrant

--- init2.sh
+++ init2.sh
@@ -5,3 +5,7 @@ for i in lib/cni lib/rancher log/containers log/pods ; do
   mkdir -p "/mnt/data/var/$i"
   ln -s "/mnt/data/var/$i" "/var/$i"
 done
+
+# Mount the local folder at /vagrant
+mkdir -p /vagrant
+mount.vboxsf -o uid=$(id -u bargee),gid=$(id -g bargee) vagrant /vagrant
